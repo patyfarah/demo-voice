842855cc5b73ecc074e4ab2eb31cd85aea9f14b3
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-03-24 20:11:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ def audio_to_text(audio_path):
     with open(audio_path, 'rb') as file:
         transcription = client.audio.translations.create(
             file=(audio_path, file.read()),
-            model='whisper-large-v3-turbo',
+            model='whisper-large-v3',
+            language='ar'
         )
     return transcription.text
 
