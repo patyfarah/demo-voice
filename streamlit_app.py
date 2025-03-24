3adcd72e073803bde5f59d3b992fb6e6d310db49
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-03-24 19:55:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ def frontend():
         status_placeholder.write("حفظ التسجيل...")
         transcription = audio_to_text("temp_audio.wav")
         status_placeholder.write("ترجمة التسجيل.")
-        st.text_area(transcription)
+        st.text_area("",transcription)
         
 # Function to convert audio data to audio file
 def data_to_file(recorded_audio):
