69d5242d961baefefaf834c7fa46345a7c1bc68e
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-03-25 07:25:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,12 +34,12 @@ st.markdown(
 
     /* Sidebar title font size */
     .css-1wa3e2d {
-        font-size: 8px;  /* Adjust the font size of the title */
+        font-size: 30px;  /* Adjust the font size of the title */
     }
 
     /* Sidebar text font size */
     .css-1v0mbdj {
-        font-size: 4px;  /* Adjust the font size of the markdown text */
+        font-size: 18px;  /* Adjust the font size of the markdown text */
     }
     </style>
     """,
